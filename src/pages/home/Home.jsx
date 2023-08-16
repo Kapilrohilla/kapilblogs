bd8dc63df84a95b1332ef5bf5b452883c096f6be
kapilrohilla <kapilrohilla2002@gmail.com>
2023-08-16 14:28:56 +0530

    basic setup for homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import Header from "../../component/Header/Header";
+import Posts from "../../component/Posts/Posts";
+import Sidebar from "../../component/Sidebar/Sidebar";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home">
+    <>
       <Header />
-      home
-    </div>
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   );
 };
 

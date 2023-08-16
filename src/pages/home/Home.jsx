0d49666a2cf384f7b4ba0067f89d4f2c880a22ea
kapilrohilla <kapilrohilla2002@gmail.com>
2023-08-16 11:43:38 +0530

    header

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../component/Header/Header";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      home
+    </div>
+  );
+};
+
+export default Home;
